865fea951a225398121b25acf51f092194271ca1
root <root@ks214156.kimsufi.com>
2014-12-28 17:13:58 +0100

    google analytics code

--- header.php
+++ header.php
@@ -22,6 +22,19 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- GOOGLE ANALYTICS -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58046278-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
