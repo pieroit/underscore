989ace6bac446d89edc72a454b3e4b0ff53a0c80
pieroit <savastanos@libero.it>
2015-02-17 17:30:50 +0100

    shortcodes for translation

--- content-single.php
+++ content-single.php
@@ -7,7 +7,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
+                <?php echo do_shortcode('[ptranslate dimension="small"]'); ?>
+            
 		<div class="entry-meta">
 			<?php underscore_posted_on(); ?>
 		</div><!-- .entry-meta -->
