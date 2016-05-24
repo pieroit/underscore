9cd8f9e9ba4e739b054bfa415534c6794a94b0bb
pieroit <piero.savastano@gmail.com>
2016-05-24 15:01:53 +0200

    fixes

--- content.php
+++ content.php
@@ -23,7 +23,7 @@
 		</a>
 		<?php the_excerpt(); ?>
 		<?php if( ! wp_is_mobile() ) : ?>
-			<?php echo get_the_post_thumbnail( get_the_ID(), 'large' ); ?>
+			<?php echo get_the_post_thumbnail( get_the_ID(), 'medium' ); ?>
 		<?php endif; ?>
 		<!--<a href=<?php  the_permalink(); ?>>
 			Read more...
